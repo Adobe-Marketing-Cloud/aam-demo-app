a3c18c6b0fad6381e858c00b319c0764938c95e1
Jorge Parga <parga@adobe.com>
2017-10-02 11:51:08 -0400

    api-module-refactor made the api module to behave as a middleware as a step to make the entire AAM authentication a reusable component.

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ var passport = require('passport');
 var passportOAuth2 = require('passport-oauth2');
 var request = require('request');
 var session = require('express-session');
-var winston = require('winston');
-var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({'timestamp': true})
-    ]
-});
-
+var WinstonConsole = require('winston').transports.Console;
+var WinstonLogger = require('winston').Logger;
+var bodyParser = require('body-parser');
 
 global.app_root = path.resolve(__dirname);
 global.web_root = '/';
 global.port = config.has('server.port') ? config.get('server.port') : 9000;
 
+var logger = new WinstonLogger({
+    transports: [
+        new WinstonConsole({'timestamp': true})
+    ]
+});
+
+var aamApiAuthenticationMiddleware = require(app_root + '/server/api');
 
 // set up the authentication stuff
 require(app_root + '/server/auth')(app, config, logger, passport, passportOAuth2, request, session);
@@ -26,8 +29,8 @@ require(app_root + '/server/auth')(app, config, logger, passport, passportOAuth2
 // set up server side logging
 require(app_root + '/server/logging')(app, config, logger);
 
-// set up api proxy
-require(app_root + '/server/api')(app, config, logger, request);
+// add authentication to all request
+app.all('/api/*', aamApiAuthenticationMiddleware(config, request))
 
 // set up routes
 require(app_root + '/server/routes')(app, config, logger, express, passport);
