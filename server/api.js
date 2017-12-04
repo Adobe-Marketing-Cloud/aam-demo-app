1109d669cb3afdce55139de0184c0a391c523093
Jorge Parga <el.parga@gmail.com>
2017-12-04 13:23:31 -0500

    api-module-refactor made the api module to behave as a middleware as a step to make the entire AAM authentication a reusable component. (#1)

--- server/api.js
+++ server/api.js
@@ -1,50 +1,101 @@
+function handleRequest({req, res, config, request}) {
+    if (!req.isAuthenticated()) {
+        return res.sendStatus(401);;
+    }
 
-module.exports = function(app, config, logger, request) {
-
-    // proxy all api calls to api server after adding bearer token
-    app.all( '/api/*', function( req, res ) {
-
-        var authorizationHeader = '';
-        var endpoint = req.params[0];
-
-        if (req.isAuthenticated()) {
-            authorizationHeader = 'Bearer ' + req.user.accessToken;
-            if (req.user.tokenType === 'IMS') {
-                //FIXME: add the orgId in api path if token type is IMS
-                endpoint = endpoint + 'org/' + user.imsOrgId;
-            }
-        } else {
-            res.sendStatus(401);
-            return;
-        }
-
-        var data = {
-            url: config.get('api.host') + config.get('api.basePath') + '/' + endpoint,
-            qs: req.query,
-            method: req.method,
-            headers: {
-                'Authorization': authorizationHeader,
-                'Content-Type': 'application/json'
-            }
+    var options = {};
+    if (req.method === 'POST' || req.method === 'PUT') {
+        options = {
+            ...options,
+            end: false
         };
-        var options = {};
-        if (req.method === 'POST' || req.method === 'PUT') {
-            data.json = true;
-            data.body = req.body;
-            data.headers['Content-Length'] = JSON.stringify(req.body).length;
-            options.end = false;
-        }
-        var callback = function(error, response, body){
-            if (error) {
-                console.error(error.message);
-                //throw error;
-                res.sendStatus(500);
-            }
-        };
-        req.pipe( request(data, callback), options ).pipe( res );
+    }
 
+    return req
+    .pipe(request(
+        getPayloadWithBearer(req, config),
+        (error) => onError(res, error)
+    ), options)
+    .pipe(res);
+}
+
+function getPayloadWithBearer(req, config) {
+    const {
+        query,
+        method,
+        user,
+        body,
+        params
+    } = req;
+    const authorizationHeader = `Bearer ${user.accessToken}`;
+
+    const endpoint = generateEndpoint(params[0], user);
+    const url = getUrl({
+        host: config.get('api.host'),
+        basePath: config.get('api.basePath'),
+        endpoint
     });
 
+    const payload = generatePayload({
+        url,
+        method,
+        query,
+        authorizationHeader,
+        body
+    });
+
+    return payload;
+}
+
+function generateEndpoint(requestEndpoint, {tokenType, imsOrgId}) {
+    if (tokenType === 'IMS') {
+        //FIXME: add the orgId in api path if token type is IMS
+        return `${requestEndpoint}org/${imsOrgId}`;
+    }
+
+    return requestEndpoint;
+}
+
+function getUrl({host, basePath, endpoint}) {
+    return `${host}${basePath}/${endpoint}`;
+}
+
+function generatePayload({url, method, query, authorizationHeader, body}) {
+    let payload = {
+        url,
+        method,
+        qs: query,
+        headers: {
+            'Authorization': authorizationHeader,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    if (method === 'POST' || method === 'PUT') {
+        payload = addPostPutConfig(payload, body);
+    }
+
+    return payload;
+}
+
+function addPostPutConfig(payload, body) {
+    let payloadWithPostPutConfig = {
+        ...payload,
+        body,
+        json: true,
+    };
+
+    payloadWithPostPutConfig.headers['Content-Length'] = JSON.stringify(body).length;
+
+    return payloadWithPostPutConfig;
+}
+
+function onError (res, error) {
+    if (error) {
+        console.error(error.message);
+        //throw error;
+        res.sendStatus(500);
+    }
 };
 
-
+module.exports = (config, request) => (req, res) => handleRequest({req, res, config, request});
